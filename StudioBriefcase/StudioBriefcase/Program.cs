b253f363c93dced7ffb0fac72f8feb49c0ff633e
Tyler Reber <tareber@student.fullsail.edu>
2023-07-03 20:58:24 -0500

    Managed ID in Azure
    I downloaded the profile and published.
    
    O-Auth went live prematurely. It works, just the login and logout link reference a local host address.

--- StudioBriefcase/StudioBriefcase/Program.cs
+++ StudioBriefcase/StudioBriefcase/Program.cs
@@ -19,30 +19,16 @@ var builder = WebApplication.CreateBuilder(args);
 // KeyVault ID and Secrets belong to an newly registered application in azure
 var keyVaultURL = builder.Configuration.GetSection("KeyVault:KeyVaultURL");
 var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(new AzureServiceTokenProvider().KeyVaultTokenCallback));
-var keyVaultClientID = builder.Configuration.GetSection("KeyVault:ClientId");
-var keyVaultClientSecret = builder.Configuration.GetSection("KeyVault:ClientSecret");
-var KeyvaultDirectoryID = builder.Configuration.GetSection("KeyVault:DirectoryID");
-//Create the client's Credentials object needed to access the Azure Keyvault
-var credential = new ClientSecretCredential(
-    KeyvaultDirectoryID.Value!.ToString(), 
-    keyVaultClientID.Value!.ToString(), 
-    keyVaultClientSecret.Value!.ToString());
 
 // The New Azure application is added to the keyvault permissions before it can read from the key vault.
 builder.Configuration.AddAzureKeyVault(
     keyVaultURL.Value!.ToString(), 
-    keyVaultClientID.Value!.ToString(), 
-    keyVaultClientSecret.Value!.ToString(), 
     new DefaultKeyVaultSecretManager());
 //Create a Client with the credentials that can retieve From the vault
-var client = new SecretClient(new Uri(keyVaultURL.Value!.ToString()), credential);
+var client = new SecretClient(new Uri(keyVaultURL.Value!.ToString()), new DefaultAzureCredential());
 
 string id = client.GetSecret("GitHub-clientid").Value.Value.ToString();
 string secret = client.GetSecret("GitHub-clientsecret").Value.Value.ToString();
-//builder.Services.AddDbContext<SampleDatabaseContext>(Options =>
-//{
-//    Options.UseSqlServer(client.GetSecret("ProdConnection").Value.Value.ToString());
-//});
 
 
 builder.Services.AddRazorPages();
@@ -77,12 +63,6 @@ builder.Services.AddAuthentication("cookie")
     };
 });
 
-//builder.Configuration.AddJsonFile("appsettings.json");
-//builder.Configuration.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
-//optional:true, reloadOnChange: true).AddEnvironmentVariables();
-
-
-
 var app = builder.Build();
 
 // Configure the http request pipeline
@@ -111,26 +91,9 @@ app.MapGet("/login", (HttpContext ctx) =>
     return Results.Challenge(new AuthenticationProperties()
     {
         
-        RedirectUri = "https://localhost:7166"
+        RedirectUri = "/signin-github"
     },
     authenticationSchemes: new List<string>() { "github" }); ;
 });
-
-//Tutorial Example
-// Output from this code Displays page with just a list which looks like []
-//app.MapGet("/", (HttpContext ctx) =>
-//{
-//    //This line populates the[list] with the claims returns from signing in with github.
-//    return ctx.User.Claims.Select(x => new { x.Type, x.Value }).ToList();
-//});
-
-//app.MapGet("/login", () =>
-//{
-//    return Results.Challenge(new AuthenticationProperties()
-//    {
-//        RedirectUri = "https://localhost:7166"
-//    },
-//    authenticationSchemes: new List<string>() { "github" });
-//});
 
 app.Run();
